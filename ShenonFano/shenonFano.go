40c9e0cb0eb9e6cb0d8f6aa93f854d2b1717a0d4
rmnvlv <romario2000@bk.ru>
2021-11-18 02:27:08 +0300

    good 2 slices :]

--- ShenonFano/shenonFano.go
+++ ShenonFano/shenonFano.go
@@ -2,42 +2,54 @@ package main
 
 import (
 	"fmt"
-	//"math"
 )
 
 //Функция возвращает мапу из кодов букв и частоты их появления в сообщении
-func calculateFrequancy(message string) (map[rune]int, int) {
+func calculateFrequancy(message string) map[rune]int {
 
 	characterFrequency := make(map[rune]int)
-	countWords := 0
 
 	for _, char := range message {
 		if _, ok := characterFrequency[char]; ok {
 			characterFrequency[char] += 1
-			countWords += 1
 		} else {
 			characterFrequency[char] = 1
 		}
 	}
 
 	fmt.Println(characterFrequency, " мапа сообщения")
-	fmt.Println(countWords, " Всего ьукв алфавит")
 	calculateProbability(characterFrequency, message)
 
-	return characterFrequency, countWords
+	return characterFrequency
 }
 
 //Разбивает мапу на 2 массива и сортирует значения в порядке возрастания
 func calculateProbability(characterFrequency map[rune]int, message string) /*([]int, []rune)*/ {
-
 	lengthOfMessage := len(message)
-
 	charMas := make([]rune, 0, lengthOfMessage)
 	freqMas := make([]int, 0, lengthOfMessage)
 
+	//Разбивает
 	for char, freq := range characterFrequency {
-		charMas = append(charMas, char)
-		freqMas = append(freqMas, freq)
+
+		if len(freqMas) == 0 || freqMas[len(freqMas)-1] < freq {
+			freqMas = append(freqMas, freq)
+			charMas = append(charMas, char)
+			continue
+		}
+
+		//fmt.Println("Начало цикла сортировки")
+		for i := 0; i < len(charMas); i++ {
+			if freqMas[i] >= freq {
+				freqMas = append(freqMas[:i], append([]int{freq}, freqMas[i:]...)...)
+				charMas = append(charMas[:i], append([]rune{char}, charMas[i:]...)...)
+				break
+			} /*else {
+				freqMas = append(freqMas, freq)
+				charMas = append(charMas, char)
+				break
+			}*/
+		}
 	}
 
 	fmt.Println(charMas, " Это массив чаров")
@@ -46,6 +58,7 @@ func calculateProbability(characterFrequency map[rune]int, message string) /*([]
 }
 
 func main() {
+
 	var message string
 	fmt.Scanln(&message)
 	calculateFrequancy(message)
